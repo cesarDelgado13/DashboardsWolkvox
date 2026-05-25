93963cb4957573386706855f5564105f925af0e2
cesarDelgado13 <ci.delgadoalfaro@ugto.mx>
2026-05-25 17:13:26 -0600

    corrigiendo merge por conn_id

--- functions.py
+++ functions.py
@@ -106,6 +106,7 @@ def campaign_3(mes, file=False):
             logger("Creando archivo de salida:",os.path.join(path,datetime.now().strftime("Llamadas_%Y%m%d.xlsx")))
             data.to_excel(os.path.join(path,datetime.now().strftime("Llamadas_%Y%m%d.xlsx")))
     else:
+        logger(f"No se encontraron registros para {date_ini} - {date_end}")
         data = 0
     logger("Extraccion finalizada")
     return data
@@ -120,7 +121,7 @@ def diagram_reports_1(mes, file=False):
     
     Regresa un DataFrame con la data encontrada
     '''
-    logger("Ejecución reporte Diagram Reports - 1. Detalle de llamadas IVR, de Diagram Reports")
+    logger("Ejecución reporte Diagram Reports - '1. Detalle de llamadas IVR'")
     date_ini, date_end = control_fechas(mes)
   
     try:
@@ -129,8 +130,6 @@ def diagram_reports_1(mes, file=False):
         response = requests.request("GET", url, headers=headers, data={})
         if response.status_code == 200:
             data = pd.DataFrame(data=response.json()["data"])
-            logger(f"Data obtenida {len(data)} registros")
-            logger(data.head(5))
         else:
             logger(response.text)
     except Exception as exc:
@@ -147,29 +146,41 @@ def diagram_reports_1(mes, file=False):
             logger("Creando archivo de salida:",os.path.join(path,datetime.now().strftime("Llamadas_%Y%m%d.xlsx")))
             data.to_excel(os.path.join(path,datetime.now().strftime("Llamadas_%Y%m%d.xlsx")))
     else:
+        logger(f"No se encontraron registros para {date_ini} - {date_end}")
         data = 0
     
     logger("Extraccion finalizada")
     return data
     
 
-def control_llamadas(llamadas:pd.DataFrame, tipificaciones: pd.DataFrame, file = False):
-    df = llamadas.merge(tipificaciones,left_on='telephone', right_on='ani')
-    logger(df.head())
+def panel_principal(llamadas:pd.DataFrame, tipificaciones: pd.DataFrame, file = False):
+    logger("Filtrando reporte '1. Detalle de llamadas IVR' por 'IVR CONVERSACIONAL LIVERPOOL..'")
+    tipificaciones = tipificaciones[tipificaciones["rp_name"] == "IVR Conversacional LIVERPOOL"]
+    logger(f"Nueva longitud de {len(tipificaciones)}")
+    logger("Filtro terminado")
+
+    logger("Creando panel principal")
+    df = llamadas.merge(tipificaciones,left_on='conn_id', right_on='conn_id',how='left')
+
     if file:
-        path = create_path(datetime.now().strftime("data/%Y%m"))
         logger("Creando archivo de salida:",os.path.join(path,datetime.now().strftime("Llamadas_%Y%m%d.xlsx")))
+        path = create_path(datetime.now().strftime("data/%Y%m"))
         df.to_excel(os.path.join(path,datetime.now().strftime("Llamadas_%Y%m%d.xlsx")))
-    
+        logger("Archivo creado")
+
     # Obteniendo el estatus de llamada (ANSWER, ANSWER-MACHINE, BUSY, NO-ANSWER, CONGESTION, FAILED)
     conteo_estatus_llamada = df.value_counts("result_x")
-    logger(conteo_estatus_llamada)
+    logger("Total estatus llamada",conteo_estatus_llamada)
 
-    # Obteniendo el estatus de llamada (AGENTE, NO_PASA_AGENTE, CUELGA_LLAMADA, BUZON, PULSE)
+    # Obteniendo la tipificacion del IVR (AGENTE, NO_PASA_AGENTE, CUELGA_LLAMADA, BUZON, PULSE)
     conteo_tipificacion = df.value_counts("cod_opc_menu")
-    logger(conteo_tipificacion)
+    logger("Total estatus tipificacion",conteo_tipificacion)
+    
+    # Obteniendo el total de registros unicos
+    total_registros = df["telephone"].nunique()
+    logger(f"Total registros unicos {total_registros}")
 
 llamadas = campaign_3("5")
 tipificaciones = diagram_reports_1("5")
 
-control_llamadas(llamadas,tipificaciones)
+panel_principal(llamadas,tipificaciones, True)
