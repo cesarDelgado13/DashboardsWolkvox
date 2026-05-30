dc6b91855cfb46f8b6d5c88c62ceb115cd50d9db
cesarDelgado13 <ci.delgadoalfaro@ugto.mx>
2026-05-29 18:36:27 -0600

    creando segunda grafica

--- app/routes/wolkvox.py
+++ app/routes/wolkvox.py
@@ -4,23 +4,36 @@ import pandas as pd
 from fastapi import APIRouter, Form, UploadFile, File
 from fastapi.responses import JSONResponse
 
-from app.services.functions import panel_principal
+from app.services.functions import panel_principal, get_details_tipificacion, get_details_numero
 from app.services.wolkvox_api import ejecutar_accion
 
 router = APIRouter()
+
+@router.get("/ivr-status/{estatus}")
+async def detalle_estatus(estatus: str):
+    df = get_details_tipificacion(5,estatus)    
+    return df.to_dict(orient="records")
+
+@router.get("/bot-status/{estatus}")
+async def detalle_estatus(estatus: str):
+    df = get_details_numero(5,estatus)    
+    return df.to_dict(orient="records")
+
 @router.get("/panel-principal")
 async def obtener_grafica_estatus():
     # ... Aquí ejecutas tu código de Pandas ...
     total_registros, conteo_estatus_llamada, conteo_tipificacion, conteo_remarcaciones = panel_principal(5)
     
     # Convertimos los datos de Pandas a listas nativas de Python
-    etiquetas = conteo_tipificacion.index.tolist()       # ['ANSWER', 'NO-ANSWER']
-    porcentajes = conteo_tipificacion["porcentaje"].tolist() # [71.4, 28.6]
+    bot_etiquetas = conteo_tipificacion.index.tolist()       # ['CV_AGENTE','CV_CUELGA_LLAMADA']
+    bot_porcentajes = conteo_tipificacion["porcentaje"].tolist() # [71.4, 28.6]
     
     return {
         "status": "success",
-        "labels": etiquetas,
-        "data": porcentajes
+        "data": {
+            "bot_etiquetas" : bot_etiquetas,
+            "bot_porcentajes" : bot_porcentajes
+        }
     }
 
 
